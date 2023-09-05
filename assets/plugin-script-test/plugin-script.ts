2a73f8a50b8f31c1ad8b6904894a8278b959b065
PP_Pro <chinese1236@qq.com>
2023-09-05 17:38:47 +0800

    add test for comp in editor plugin

--- assets/plugin-script-test/plugin-script.ts
+++ assets/plugin-script-test/plugin-script.ts
@@ -1,11 +1,15 @@
 import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
+import { assert } from '../assert';
 
 @ccclass('plugin_script')
 export class plugin_script extends Component {
     start() {
         const testComp = this.addComponent('CompInPluginScript');
-        console.log(testComp)
+        assert(!!testComp, 'Failed to register component in plugin script');
+
+        const testCompInEditorPlugin = this.addComponent('component_in_editor_plugin');
+        assert(!!testCompInEditorPlugin, 'Failed to register component in editor plugin');
     }
 
     update(deltaTime: number) {
---
+++ extensions/script-invoker/assets/component-in-editor-plugin.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('component_in_editor_plugin')
+export class component_in_editor_plugin extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
